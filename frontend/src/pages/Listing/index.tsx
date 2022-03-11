d5c59c3a3900470faa712033844cc87d3a12f8b3
Frederico Rodarte <fredericorodarte12@gmail.com>
2022-03-11 20:24:12 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,32 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination/Index";
 
 function Listing() {
     return (
-        <Pagination />
+        <>
+            <Pagination />
+
+            <div className="cotainer">
+                <div className="row">
+                    <div className="col-lg-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-lg-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-lg-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-lg-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-lg-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>                    
+                </div>
+            </div>
+
+        </>
     )
 }
 
